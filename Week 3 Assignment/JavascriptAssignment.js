4a464b6902febe75b58b3d5928f66f9dc302c1ee
Walter Ebuka Onyekwelu <onyekweluwalter@gmail.com>
2024-07-23 23:40:41 +0100

    Add files via upload

---
+++ Week 3 Assignment/JavascriptAssignment.js
@@ -0,0 +1,34 @@
+// Create five different variables for each of the following types:
+// Strings
+// Numbers
+// Booleans
+// Null
+
+// 5 strings
+var statement1 = "It's alright";
+var statement2 = "He is called 'Johnny'";
+var statement3 = 'He is called "Johnson"';
+var statement4 = "Congratulation! You have Passed your test";
+var statement5 = 'Thanks for contacting us today.'
+
+// 5 Numbers
+var a = 20;
+var b = 40;
+var c = 50;
+var d = 60;
+var e = 80;
+
+// 5 Booleans 
+
+console.log(Boolean(statement1 == a));
+console.log(Boolean(statement2 == "He is called 'Johnny'"));
+console.log(Boolean(statement3 == "It's alright"));
+console.log(Boolean(b == 50));
+console.log(Boolean(statement5 =='Thanks for contacting us today.'));
+
+// 5 Null
+let myVar = null;
+let Debit = null;
+let Credit = null;
+let myCar = null;
+let myName = null;
